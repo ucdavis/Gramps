b1bd10e8f6e0771eea0b4d9e15b2b72497dcff84
Sylvestre, Jason <jsylvestre@ucdavis.edu>
2011-03-29 16:34:40 +0000

    Code Cleanup, Added AccountController tests
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/GRAMPS/TRUNK/GRAMPS;C9711

---
+++ Gramps.Tests/Controllers/AccountControllerTests.cs
@@ -0,0 +1,241 @@
+using System;
+using System.Linq;
+using System.Web.Routing;
+using Gramps.Controllers;
+using Gramps.Controllers.Filters;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MvcContrib.TestHelper;
+using UCDArch.Testing;
+using UCDArch.Web.Attributes;
+
+namespace Gramps.Tests.Controllers
+{
+    [TestClass]
+    public class AccountControllerTests : ControllerTestBase<AccountController>
+    {
+        private readonly Type _controllerClass = typeof(AccountController);
+
+        #region Init
+        /// <summary>
+        /// Setups the controller.
+        /// </summary>
+        protected override void SetupController()
+        {
+            Controller = new TestControllerBuilder().CreateController<AccountController>();
+        }
+
+        protected override void RegisterRoutes()
+        {
+            RouteRegistrar.RegisterRoutes(RouteTable.Routes);
+        }
+        #endregion Init
+
+        #region Mapping Tests
+
+        /// <summary>
+        /// #1
+        /// </summary>
+        [TestMethod]
+        public void TestLogOnMapping()
+        {
+            "~/Account/LogOn/".ShouldMapTo<AccountController>(a => a.LogOn(null));
+        }
+
+        /// <summary>
+        /// #2
+        /// </summary>
+        [TestMethod]
+        public void TestLogOutMapping()
+        {
+            "~/Account/LogOut/".ShouldMapTo<AccountController>(a => a.LogOut());
+        }
+
+        #endregion Mapping Tests
+
+        #region Reflection Tests
+
+        #region Controller Class Tests
+        /// <summary>
+        /// Tests the controller inherits from application controller.
+        /// </summary>
+        [TestMethod]
+        public void TestControllerInheritsFromApplicationController()
+        {
+            #region Arrange
+            var controllerClass = _controllerClass;
+            #endregion Arrange
+
+            #region Act
+            Assert.IsNotNull(controllerClass.BaseType);
+            var result = controllerClass.BaseType.Name;
+            #endregion Act
+
+            #region Assert
+            Assert.AreEqual("ApplicationController", result);
+            #endregion Assert
+        }
+
+        /// <summary>
+        /// Tests the controller inherits from super controller.
+        /// </summary>
+        [TestMethod]
+        public void TestControllerInheritsFromSuperController()
+        {
+            #region Arrange
+            var controllerClass = _controllerClass;
+            #endregion Arrange
+
+            #region Act
+            Assert.IsNotNull(controllerClass.BaseType);
+            Assert.IsNotNull(controllerClass.BaseType.BaseType);
+            var result = controllerClass.BaseType.BaseType.Name;
+            #endregion Act
+
+            #region Assert
+            Assert.AreEqual("SuperController", result);
+            #endregion Assert
+        }
+
+        /// <summary>
+        /// Tests the controller has only three attributes.
+        /// </summary>
+        [TestMethod]
+        public void TestControllerHasOnlyThreeAttributes()
+        {
+            #region Arrange
+            var controllerClass = _controllerClass;
+            #endregion Arrange
+
+            #region Act
+            var result = controllerClass.GetCustomAttributes(true);
+            #endregion Act
+
+            #region Assert
+            Assert.AreEqual(3, result.Count());
+            #endregion Assert
+        }
+
+        /// <summary>
+        /// Tests the controller has transaction attribute.
+        /// </summary>
+        [TestMethod]
+        public void TestControllerHasTransactionAttribute()
+        {
+            #region Arrange
+            var controllerClass = _controllerClass;
+            #endregion Arrange
+
+            #region Act
+            var result = controllerClass.GetCustomAttributes(true).OfType<UseTransactionsByDefaultAttribute>();
+            #endregion Act
+
+            #region Assert
+            Assert.IsTrue(result.Count() > 0, "UseTransactionsByDefaultAttribute not found.");
+            #endregion Assert
+        }
+
+        /// <summary>
+        /// Tests the controller has anti forgery token attribute.
+        /// </summary>
+        [TestMethod]
+        public void TestControllerHasAntiForgeryTokenAttribute()
+        {
+            #region Arrange
+            var controllerClass = _controllerClass;
+            #endregion Arrange
+
+            #region Act
+            var result = controllerClass.GetCustomAttributes(true).OfType<UseAntiForgeryTokenOnPostByDefault>();
+            #endregion Act
+
+            #region Assert
+            Assert.IsTrue(result.Count() > 0, "UseAntiForgeryTokenOnPostByDefault not found.");
+            #endregion Assert
+        }
+
+        [TestMethod]
+        public void TestControllerHasVersionAttribute()
+        {
+            #region Arrange
+            var controllerClass = _controllerClass;
+            #endregion Arrange
+
+            #region Act
+            var result = controllerClass.GetCustomAttributes(true).OfType<VersionAttribute>();
+            #endregion Act
+
+            #region Assert
+            Assert.IsTrue(result.Count() > 0, "VersionAttribute not found.");
+            #endregion Assert
+        }
+
+        #endregion Controller Class Tests
+
+        #region Controller Method Tests
+
+        [TestMethod]
+        public void TestControllerContainsExpectedNumberOfPublicMethods()
+        {
+            #region Arrange
+            var controllerClass = _controllerClass;
+            #endregion Arrange
+
+            #region Act
+            var result = controllerClass.GetMethods().Where(a => a.DeclaringType == controllerClass);
+            #endregion Act
+
+            #region Assert
+            Assert.AreEqual(2, result.Count(), "It looks like a method was added or removed from the controller.");
+            #endregion Assert
+        }
+
+        /// <summary>
+        /// #1
+        /// </summary>
+        [TestMethod]
+        public void TestControllerMethodLogOnContainsExpectedAttributes()
+        {
+            #region Arrange
+            var controllerClass = _controllerClass;
+            var controllerMethod = controllerClass.GetMethod("LogOn");
+            #endregion Arrange
+
+            #region Act
+            //var expectedAttribute = controllerMethod.GetCustomAttributes(true).OfType<UserOnlyAttribute>();
+            var allAttributes = controllerMethod.GetCustomAttributes(true);
+            #endregion Act
+
+            #region Assert
+            //Assert.AreEqual(1, expectedAttribute.Count(), "UserOnlyAttribute not found");
+            Assert.AreEqual(0, allAttributes.Count());
+            #endregion Assert
+        }
+
+        /// <summary>
+        /// #2
+        /// </summary>
+        [TestMethod]
+        public void TestControllerMethodLogOutContainsExpectedAttributes()
+        {
+            #region Arrange
+            var controllerClass = _controllerClass;
+            var controllerMethod = controllerClass.GetMethod("LogOut");
+            #endregion Arrange
+
+            #region Act
+            //var expectedAttribute = controllerMethod.GetCustomAttributes(true).OfType<UserOnlyAttribute>();
+            var allAttributes = controllerMethod.GetCustomAttributes(true);
+            #endregion Act
+
+            #region Assert
+            //Assert.AreEqual(1, expectedAttribute.Count(), "UserOnlyAttribute not found");
+            Assert.AreEqual(0, allAttributes.Count());
+            #endregion Assert
+        }
+
+
+        #endregion Controller Method Tests
+
+        #endregion Reflection Tests
+    }
+}
--- Gramps/Controllers/AccountController.cs
+++ Gramps/Controllers/AccountController.cs
@@ -1,12 +1,16 @@
 ﻿using System.Web.Mvc;
 using System.Web.Security;
 using UCDArch.Web.Authentication;
-using UCDArch.Web.Controller;
 
 namespace Gramps.Controllers
 {
     public class AccountController : ApplicationController
     {
+        /// <summary>
+        /// #1
+        /// </summary>
+        /// <param name="returnUrl"></param>
+        /// <returns></returns>
         public ActionResult LogOn(string returnUrl)
         {
             string resultUrl = CASHelper.Login(); //Do the CAS Login
@@ -22,6 +26,10 @@ namespace Gramps.Controllers
             return View();
         }
 
+        /// <summary>
+        /// #2
+        /// </summary>
+        /// <returns></returns>
         public ActionResult LogOut()
         {
             FormsAuthentication.SignOut();
