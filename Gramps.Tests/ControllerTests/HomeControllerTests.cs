00b12ffbdd8d47b06e9e81dfcfec78755a573f59
Sylvestre, Jason <jsylvestre@ucdavis.edu>
2011-04-14 19:36:36 +0000

    Added HomeController Tests
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/GRAMPS/TRUNK/GRAMPS;C9787

---
+++ Gramps.Tests/ControllerTests/HomeControllerTests.cs
@@ -0,0 +1,327 @@
+using System;
+using System.Linq;
+using System.Web.Routing;
+using Gramps.Controllers;
+using Gramps.Controllers.Filters;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MvcContrib.TestHelper;
+using UCDArch.Testing;
+using UCDArch.Web.Attributes;
+
+
+namespace Gramps.Tests.ControllerTests
+{
+    [TestClass]
+    public class HomeControllerTests : ControllerTestBase<HomeController>
+    {
+        private readonly Type _controllerClass = typeof(HomeController);
+
+        #region Init
+        /// <summary>
+        /// Setups the controller.
+        /// </summary>
+        protected override void SetupController()
+        {
+            Controller = new TestControllerBuilder().CreateController<HomeController>();
+        }
+
+        protected override void RegisterRoutes()
+        {
+            RouteRegistrar.RegisterRoutes(RouteTable.Routes);
+        }
+
+        #endregion Init
+
+        #region Mapping Tests
+        /// <summary>
+        /// #1
+        /// </summary>
+        [TestMethod]
+        public void TestIndexMapping1()
+        {
+            "~/Home/Index/".ShouldMapTo<HomeController>(a => a.Index());
+        }
+
+        /// <summary>
+        /// #1
+        /// </summary>
+        [TestMethod]
+        public void TestIndexMapping2()
+        {
+            "~/Home".ShouldMapTo<HomeController>(a => a.Index());
+        }
+
+        /// <summary>
+        /// #2
+        /// </summary>
+        [TestMethod]
+        public void TestAboutMapping()
+        {
+            "~/Home/About".ShouldMapTo<HomeController>(a => a.About());
+        }
+
+        /// <summary>
+        /// #3
+        /// </summary>
+        [TestMethod]
+        public void TestLoggedOutMapping()
+        {
+            "~/Home/LoggedOut".ShouldMapTo<HomeController>(a => a.LoggedOut());
+        }
+        #endregion Mapping Tests
+
+        #region Method Tests
+
+        /// <summary>
+        /// #1
+        /// </summary>
+        [TestMethod]
+        public void TestIndexReturnsView()
+        {
+            #region Arrange
+            
+            #endregion Arrange
+
+            #region Act
+            Controller.Index()
+                .AssertViewRendered();
+            #endregion Act
+
+            #region Assert
+            #endregion Assert		
+        }
+
+
+        [TestMethod]
+        public void TestAboutReturnsView()
+        {
+            #region Arrange
+            
+            #endregion Arrange
+
+            #region Act
+            Controller.About()
+                .AssertViewRendered();
+            #endregion Act
+
+            #region Assert
+            #endregion Assert		
+        }
+
+
+        [TestMethod]
+        public void TestLoggedOutReturnsView()
+        {
+            #region Arrange
+            
+            #endregion Arrange
+
+            #region Act
+            Controller.LoggedOut()
+                .AssertViewRendered();
+            #endregion Act
+
+            #region Assert
+            #endregion Assert		
+        }
+        #endregion Method Tests
+
+        #region Reflection Tests
+
+        #region Controller Class Tests
+        /// <summary>
+        /// Tests the controller inherits from application controller.
+        /// </summary>
+        [TestMethod]
+        public void TestControllerInheritsFromApplicationController()
+        {
+            #region Arrange
+            var controllerClass = _controllerClass;
+            #endregion Arrange
+
+            #region Act
+            Assert.IsNotNull(controllerClass.BaseType);
+            var result = controllerClass.BaseType.Name;
+            #endregion Act
+
+            #region Assert
+            Assert.AreEqual("ApplicationController", result);
+            #endregion Assert
+        }
+
+        /// <summary>
+        /// Tests the controller has four attributes.
+        /// </summary>
+        [TestMethod]
+        public void TestControllerHasFourAttributes()
+        {
+            #region Arrange
+            var controllerClass = _controllerClass;
+            #endregion Arrange
+
+            #region Act
+            var result = controllerClass.GetCustomAttributes(true);
+            #endregion Act
+
+            #region Assert
+            Assert.AreEqual(4, result.Count());
+            #endregion Assert
+        }
+
+        /// <summary>
+        /// Tests the controller has transaction attribute.
+        /// </summary>
+        [TestMethod]
+        public void TestControllerHasTransactionAttribute()
+        {
+            #region Arrange
+            var controllerClass = _controllerClass;
+            #endregion Arrange
+
+            #region Act
+            var result = controllerClass.GetCustomAttributes(true).OfType<UseTransactionsByDefaultAttribute>();
+            #endregion Act
+
+            #region Assert
+            Assert.IsTrue(result.Count() > 0, "UseTransactionsByDefaultAttribute not found.");
+            #endregion Assert
+        }
+
+        /// <summary>
+        /// Tests the controller has anti forgery token attribute.
+        /// </summary>
+        [TestMethod]
+        public void TestControllerHasAntiForgeryTokenAttribute()
+        {
+            #region Arrange
+            var controllerClass = _controllerClass;
+            #endregion Arrange
+
+            #region Act
+            var result = controllerClass.GetCustomAttributes(true).OfType<UseAntiForgeryTokenOnPostByDefault>();
+            #endregion Act
+
+            #region Assert
+            Assert.IsTrue(result.Count() > 0, "UseAntiForgeryTokenOnPostByDefault not found.");
+            #endregion Assert
+        }
+
+        [TestMethod]
+        public void TestControllerHasVersionAttribute()
+        {
+            #region Arrange
+            var controllerClass = _controllerClass;
+            #endregion Arrange
+
+            #region Act
+            var result = controllerClass.GetCustomAttributes(true).OfType<VersionAttribute>();
+            #endregion Act
+
+            #region Assert
+            Assert.IsTrue(result.Count() > 0, "VersionAttribute not found.");
+            #endregion Assert
+        }
+
+        [TestMethod]
+        public void TestControllerHasHandleTransactionsManuallyAttribute()
+        {
+            #region Arrange
+            var controllerClass = _controllerClass;
+            #endregion Arrange
+
+            #region Act
+            var result = controllerClass.GetCustomAttributes(true).OfType<HandleTransactionsManuallyAttribute>();
+            #endregion Act
+
+            #region Assert
+            Assert.IsTrue(result.Count() > 0, "HandleTransactionsManuallyAttribute not found.");
+            #endregion Assert
+        }
+
+        #endregion Controller Class Tests
+
+        #region Controller Method Tests
+
+        [TestMethod]
+        public void TestControllerContainsExpectedNumberOfPublicMethods()
+        {
+            #region Arrange
+            var controllerClass = _controllerClass;
+            #endregion Arrange
+
+            #region Act
+            var result = controllerClass.GetMethods().Where(a => a.DeclaringType == controllerClass);
+            #endregion Act
+
+            #region Assert
+            Assert.AreEqual(3, result.Count(), "It looks like a method was added or removed from the controller.");
+            #endregion Assert
+        }
+
+        /// <summary>
+        /// #1
+        /// </summary>
+        [TestMethod]
+        public void TestControllerMethodIndexContainsExpectedAttributes()
+        {
+            #region Arrange
+            var controllerClass = _controllerClass;
+            var controllerMethod = controllerClass.GetMethod("Index");
+            #endregion Arrange
+
+            #region Act
+            var expectedAttribute = controllerMethod.GetCustomAttributes(true).OfType<UserOnlyAttribute>();
+            var allAttributes = controllerMethod.GetCustomAttributes(true);
+            #endregion Act
+
+            #region Assert
+            Assert.AreEqual(1, expectedAttribute.Count(), "UserOnlyAttribute not found");
+            Assert.AreEqual(1, allAttributes.Count());
+            #endregion Assert
+        }
+
+        /// <summary>
+        /// #2
+        /// </summary>
+        [TestMethod]
+        public void TestControllerMethodAboutContainsExpectedAttributes()
+        {
+            #region Arrange
+            var controllerClass = _controllerClass;
+            var controllerMethod = controllerClass.GetMethod("About");
+            #endregion Arrange
+
+            #region Act
+            var allAttributes = controllerMethod.GetCustomAttributes(true);
+            #endregion Act
+
+            #region Assert
+            Assert.AreEqual(0, allAttributes.Count());
+            #endregion Assert
+        }
+
+        /// <summary>
+        /// #3
+        /// </summary>
+        [TestMethod]
+        public void TestControllerMethodLoggedOutContainsExpectedAttributes()
+        {
+            #region Arrange
+            var controllerClass = _controllerClass;
+            var controllerMethod = controllerClass.GetMethod("LoggedOut");
+            #endregion Arrange
+
+            #region Act
+            var allAttributes = controllerMethod.GetCustomAttributes(true);
+            #endregion Act
+
+            #region Assert
+            Assert.AreEqual(0, allAttributes.Count());
+            #endregion Assert
+        }
+
+        #endregion Controller Method Tests
+
+        #endregion Reflection Tests
+    }
+}
