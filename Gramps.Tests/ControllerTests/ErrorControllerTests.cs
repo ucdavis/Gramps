753e0cc76015d05bc5b54d4edc28178eec25aac9
Sylvestre, Jason <jsylvestre@ucdavis.edu>
2011-04-14 16:48:22 +0000

    cleanup
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/GRAMPS/TRUNK/GRAMPS;C9786

--- Gramps.Tests/ControllerTests/ErrorControllerTests.cs
+++ Gramps.Tests/ControllerTests/ErrorControllerTests.cs
@@ -1,17 +1,10 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Web.Mvc;
 using System.Web.Routing;
 using Gramps.Controllers;
 using Gramps.Controllers.Filters;
-using Gramps.Core.Domain;
-using Gramps.Services;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MvcContrib.TestHelper;
-using Rhino.Mocks;
-using UCDArch.Core.PersistanceSupport;
 using UCDArch.Testing;
 using UCDArch.Web.Attributes;
 
